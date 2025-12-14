8850090056f9fbd24fa6fea7e8b0908fdf4fa0ee
pulkiiiit <pulkitshukla001@gmail.com>
2025-12-14 22:42:31 +0530

    implement user profile management features; add password change, account update, and avatar/cover image upload functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -222,9 +222,127 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
         throw new ApiError(401, error?.message || "Invalid refresh Token")
     }
 })
+
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const{oldPassword, newPassword} = req.body
+
+    const user = await User.findById(req.user?._id)
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Old Password that you have entered is incorrect")
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave: false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changes Successfully"))
+})
+
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res
+    .status(200)
+    .json(200, req.user, "Current User Fetched Successfully")
+})
+
+const updateAccountdetails = asyncHandler(async(req, res)=> {
+    const {fullName, email} = req.body
+
+    if (!fullName || !email) {
+        throw new ApiError(400, "All fields are requried")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName: fullName,
+                email : email
+            }
+        },
+        {new : true}
+    ).select("-password")
+
+
+    res
+    .status(200)
+    .json(new ApiResponse(200, user , "Account details updated successfully"))
+})
+
+const updateUserAvatar = asyncHandler(async(req, res) => {
+    
+    const avatarLocalPath = req.file?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avaar file is missing")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+    if (!avatar.url) {
+        throw new ApiError(400, "Error while uploading avatar to cloidinary")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar: avatar.url
+            }
+        },
+        {new: true }
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Avatar update successfully")
+    )
+})
+
+const updateUserCoverImage = asyncHandler(async(req, res) => {
+    
+    const coverImageLocalPath  = req.file?.path
+
+    if(!coverImageLocalPath){
+         throw new ApiError(400, "Cover Image file is missing")
+    }
+
+    const coverImage = uploadOnCloudinary(coverImageLocalPath);
+
+    if (!coverImage.url) {
+        throw new ApiError(400, "Error while uploading cover image to cloidinary")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage: coverImage.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Cover image update successfully")
+    )
+})
+
 export {
     registerUser,
     loginUser,
     logOutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountdetails,
+    updateUserAvatar,
+    updateUserCoverImage
 }
--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -1,17 +1,19 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = new Schema ({
-    subscriber : {
-        type: Schema.Types.ObjectId, // on e who is subscribing
-        ref: "User"
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // one who is subscribing
+      ref: "User",
     },
-    channel : {
-        type : Schema.Types.ObjectId, // one to whom the "sbuscriber" is subscribing
-        ref: "User"
-    }
-}, {
-    timestamps: true
-})
+    channel: {
+      type: Schema.Types.ObjectId, // one to whom the "sbuscriber" is subscribing
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-export const Subscription = mongoose.model("Subscription" , subscriptionSchema)
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
